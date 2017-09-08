4cae0d960fec08700658e53e2a4bdd6665bb9341
jcrm3594 <jon.moyer@gmail.com>
2017-09-08 12:58:45 -0400

    documenting runExample

--- R/runExample.R
+++ R/runExample.R
@@ -1,3 +1,12 @@
+#' Shiny app for running 
+#'
+#' This function runs a Shiny app that employs the \code{crtpwr.2mean},
+#' \code{crtpwr.2prop}, and \code{crtpwr.2rate}. The app can do calculations
+#' for several sets of parameters and produces graphs.
+#' 
+#' @section Authors:
+#' Jonathan Moyer (\email{jon.moyer@@gmail.com})
+
 #' @export
 runExample <- function(){
   appDir <- system.file("shiny-examples", "app", package = "clusterPower")
