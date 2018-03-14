36c410cd701533f714b0df5af79fd407ddff26b2
arbogdan <abogdan@umass.edu>
2018-03-14 12:16:23 -0400

    Summary method for "crtpwr" objects

---
+++ summary.method.crtpwr.R
@@ -0,0 +1,18 @@
+setMethod("summary",
+    signature(object = "crtpwr"),
+    function (object, ...) 
+    {
+      cat(paste0("\n", object[['overview']], "\n"))
+      cat(paste0("\nPower Estimate (alpha = ", object[['alpha']], "):\n"))
+      print(object[['power']], row.names = FALSE)
+      cat(paste0("\nExpected Difference: ", object[['difference']], "\n"))
+      cat("\nVariance Parameters:\n")
+      print(object[['variance.parms']])
+      cat("\nCluster Sizes:\n")
+      cat(paste0("  Non-Treatment (n = ", object[['n.clusters']][1], "): ", format(object[['cluster.sizes']][1]), "\n"))
+      cat(paste0("  Treatment (n = ", object[['n.clusters']][2], "): ", format(object[['cluster.sizes']][2]), "\n"))
+      cat(paste("\nMethod:", switch(object[['method']], glmm = "Generalized Linear Mixed Model", gee = "Generalized Estimating Equation"), "\n"))
+      cat("\n")
+    }
+)
+
