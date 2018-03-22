75f7017851f7807f4cee638d3a6d0473f33ce4a9
Alex Bogdan <abogdan@schoolph.umass.edu>
2018-03-22 14:06:44 -0400

    summary.method: Changed 'difference' to 'inputs'

--- summary.method.crtpwr.R
+++ summary.method.crtpwr.R
@@ -5,7 +5,7 @@ setMethod("summary",
       cat(paste0("\n", object[['overview']], "\n"))
       cat(paste0("\nPower Estimate (alpha = ", object[['alpha']], "):\n"))
       print(object[['power']], row.names = FALSE)
-      cat(paste0("\nExpected Difference: ", object[['difference']], "\n"))
+      cat(paste0("\nExpected Difference: ", object[['inputs']], "\n"))
       cat("\nVariance Parameters:\n")
       print(object[['variance.parms']])
       cat("\nCluster Sizes:\n")
