d28a34baca555c3b4df7dd29267f49be6972c764
arbogdan <abogdan@umass.edu>
2018-03-30 17:22:23 -0400

    print.method: Custom "print" method for "crtpwr"
    - Prints "message", "power" & "method"
    - Currenty only prints when print() explicitly called

---
+++ R/print.method.crtpwr.R
@@ -0,0 +1,12 @@
+setMethod("print",
+          signature(x = 'crtpwr'),
+          function (x, ...) 
+          {
+            cat(paste0("\n", x[['overview']], "\n"))
+            cat(paste0("\nPower Estimate (alpha = ", x[['alpha']], "):\n"))
+            print(x[['power']], row.names = FALSE)
+            cat(paste("\nMethod:", switch(x[['method']], glmm = "Generalized Linear Mixed Model", gee = "Generalized Estimating Equation"), "\n"))
+            cat("\n")
+          }
+)
+setClass('crtpwr')
