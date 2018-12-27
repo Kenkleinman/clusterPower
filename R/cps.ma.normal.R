67d3e4f6a8585e6c0a743e55eb3e89c9833a321e
Lexi <acbrown2453@gmail.com>
2018-12-27 11:46:16 -0500

    revised cps.ma.normal.R, see to do list at head of document

--- R/cps.ma.normal.R
+++ R/cps.ma.normal.R
@@ -1,3 +1,25 @@
+# Validation functions
+validateVariance <- function(x){
+  warning("FIXME: not actually validating variance yet")
+}
+
+is.wholenumber = function(x, tol = .Machine$double.eps^0.5)  abs(x - round(x)) < tol
+
+
+##FIXME: TO DO
+# 1. update the return values
+# 2. update the example/ man text
+# 3. input validation
+# 4. make validateVariance fxn in "validation" file
+# 5. make validate nsubjects fxn in validation file
+# 6. make a wrapper function that takes ICC or sigma/sigma_b, also format output
+# 7. make a seperate function for taking ICC, sigma, sigma_b
+# 8. rename this fxn
+# 9. write some usage examples
+# 10. debug
+# 11. testthat tests
+
+
 #' Power simulations for cluster-randomized trials: Simple Designs, Continuous Outcome.
 #'
 #' This set of functions utilize iterative simulations to determine 
@@ -13,27 +35,24 @@
 #' between-cluster variance; significance level, analytic method, progress updates, 
 #' and simulated data set output may also be specified.
 #' 
+#' # Return enough data to save the simulated datasets
+#' # FIXME: update this
 #' 
+#' @author Alexandria C. Sakrejda
+#' @author Alexander R. Bogdan
+#' 
+#' @examples # FIXME: update this too
+#' \dontrun{
+#' Put usage example here
+#' }
+#'
 #' @param nsim Number of datasets to simulate; accepts integer (required).
-#' @param nsubjects Number of subjects per treatment group; accepts either a scalar (equal cluster sizes, both groups), 
-#' a vector of length two (equal cluster sizes within groups), or a vector of length \code{sum(nclusters)} 
-#' (unequal cluster sizes within groups) (required).
-#' @param nclusters Number of clusters per group; accepts single integer or vector of length 2 for unequal number 
-#' of clusters per treatment group (required)
-#' @param difference Expected absolute treatment effect; accepts numeric (required).
-#' At least 2 of the following must be specified:
-#' @param ICC Intra-cluster correlation coefficient; accepts a value between 0 - 1
-#' @param sigma Within-cluster variance; accepts numeric
-#' @param sigma_b Between-cluster variance; accepts numeric
-#' If clusters differ between treatment groups, at least 2 of the following 
-#' must be specified:
-#' @param ICC2 Intra-cluster correlation coefficient for clusters in TREATMENT group
-#' @param sigma2 Within-cluster variance for clusters in TREATMENT group
-#' @param sigma_b2 Between-cluster variance for clusters in TREATMENT group
+#' @param nsubjects Number of subjects per treatment group; accepts a list with one entry per arm. 
+#' Each entry is a vector containing the number of subjects per cluster (required).
+#' @param means Expected absolute treatment effect for each arm; accepts a vector of length \code{narms} (required).
+#' @param sigma Within-cluster variance; accepts a vector of length \code{narms} (required).
+#' @param sigma_b Between-cluster variance; accepts a vector of length \code{narms} (required).
 #' @param alpha Significance level; default = 0.05.
-#' @param method Analytical method, either Generalized Linear Mixed Effects Model (GLMM) or 
-#' Generalized Estimating Equation (GEE). Accepts c('glmm', 'gee') (required); default = 'glmm'.
-#' @param quiet When set to FALSE, displays simulation progress and estimated completion time; default is FALSE.
 #' @param all.sim.data Option to output list of all simulated datasets; default = FALSE.
 #' 
 #' @return A list with the following components
@@ -61,200 +80,94 @@
 #'                   "clust" (Indicator for cluster)}
 #' }
 #' 
-#' @author Alexander R. Bogdan
-#' 
-#' @examples 
-#' \dontrun{
-#' normal.sim = cps.normal(nsim = 100, nsubjects = 50, nclusters = 30, difference = 30,
-#'                         ICC = 0.2, sigma = 100, alpha = 0.05, method = 'glmm', 
-#'                         quiet = FALSE, all.sim.data = FALSE)
-#' }
-#'
 #' @export
-#' 
-#' @param nsim Number of datasets to simulate; accepts integer (required).
-#' @param narms Number of arms including the control group (required).
-#' @param nsubjects Number of subjects per treatment group; accepts a list of length \code{narms} 
-#' containing vectors of length \code{nclusters} (required).
-#' @param nclusters Number of clusters per group; accepts a vector with one entry each per arm (required)
-#' @param means Expected absolute treatment effect for each arm; accepts a vector of length \code{narms} (required).
-#' @param sigma Within-cluster variance; accepts a vector of length \code{narms} (required).
-#' @param sigma_b Between-cluster variance; accepts a vector of length \code{narms} (required).
-#' @param alpha Significance level; default = 0.05.
-#' @param quiet When set to FALSE, displays simulation progress and estimated completion time; default is FALSE.
-#' @param all.sim.data Option to output list of all simulated datasets; default = FALSE.
-
-cps.ma.normal = function(nsim = NULL, nsubjects = NULL, nclusters = NULL, difference = NULL,
-                      ICC = NULL, sigma = NULL, sigma_b = NULL,
-                      alpha = 0.05, quiet = FALSE,
+cps.ma.normal.internal = function(nsim = NULL, nsubjects = NULL,
+                      means = NULL, sigma = NULL, sigma_b = NULL,
+                      alpha = 0.05,
                       all.sim.data = FALSE){
 
   # Create vectors to collect iteration-specific values
-  est.vector = NULL
-  se.vector = NULL
-  stat.vector = NULL
-  pval.vector = NULL
+  est.vector = vector(mode = "numeric", length = nsim)
+  se.vector = vector(mode = "numeric", length = nsim)
+  stat.vector = vector(mode = "numeric", length = nsim)
+  pval.vector = vector(mode = "numeric", length = nsim)
   simulated.datasets = list()
   
-  # Set start.time for progress iterator & initialize progress bar
-  start.time = Sys.time()
-  prog.bar =  progress::progress_bar$new(format = "(:spin) [:bar] :percent eta :eta", 
-                                         total = nsim, clear = FALSE, width = 100)
-  prog.bar$tick(0)
+  # Create NCLUSTERS, NARMS, from NSUBJECTS
+  narms = length(nsubjects)
+  nclusters = sapply(nsubjects, length)
   
-  # Create wholenumber function
-  is.wholenumber = function(x, tol = .Machine$double.eps^0.5)  abs(x - round(x)) < tol
-  
-  # Validate NSIM, NCLUSTERS, NSUBJECTS
-  sim.data.arg.list = list(nsim, nclusters, nsubjects, difference)
-  sim.data.args = unlist(lapply(sim.data.arg.list, is.null))
-  if(sum(sim.data.args) > 0){
-    stop("NSIM, NCLUSTERS, NSUBJECTS & DIFFERENCE must all be specified. Please review your input values.")
-  }
+  # FIXME: Validate NSIM, NSUBJECTS
   min1.warning = " must be an integer greater than or equal to 1"
   if(!is.wholenumber(nsim) || nsim < 1){
     stop(paste0("NSIM", min1.warning))
   }
-  if(!is.wholenumber(nclusters) || nclusters < 1){
-    stop(paste0("NCLUSTERS", min1.warning))  
-  }
-  if(!is.wholenumber(nsubjects) || nsubjects < 1){
-    stop(paste0("NSUBJECTS", min1.warning))
-  }
-  if(length(nclusters) > 2){
-    stop("NCLUSTERS can only be a scalar (equal # of clusters per group) or a vector of length 2 (unequal # of clusters per group)")
-  }
-  
-  # Set cluster sizes for treatment arm (if not already specified)
-  if(length(nclusters) == 1){
-    nclusters[2] = nclusters[1]
-  }
-  
-  # Set sample sizes for each cluster (if not already specified)
-  if(length(nsubjects) == 1){
-    nsubjects[1:sum(nclusters)] = nsubjects
-  } 
-  if(length(nsubjects) == 2){
-    nsubjects = c(rep(nsubjects[1], nclusters[1]), rep(nsubjects[2], nclusters[2]))
-  }
-  if(nclusters[1] == nclusters[2] && length(nsubjects) == nclusters[1]){
-    nsubjects = rep(nsubjects, 2)
-  }
-  if(length(nclusters) == 2 && length(nsubjects) != 1 && length(nsubjects) != sum(nclusters)){
-    stop("A cluster size must be specified for each cluster. If all cluster sizes are equal, please provide a single value for NSUBJECTS")
-  }
-  
-  # Validate DIFFERENCE, ALPHA
-  min0.warning = " must be a numeric value greater than 0"
-  if(!is.numeric(difference) || difference < 0){
-    stop("DIFFERENCE", min0.warning)
-  }
+
+  # FIXME: Validate MEANS, ALPHA
   if(!is.numeric(alpha) || alpha < 0 || alpha > 1){
     stop("ALPHA must be a numeric value between 0 - 1")
   }
   
   # Validate SIGMA, SIGMA_B
+  validateVariance(sigma)
+  validateVariance(sigma_b)
   
-  
-  
-  # Validate QUIET, ALL.SIM.DATA
-  if(!is.logical(quiet)){
-    stop("QUIET must be either TRUE (No progress information shown) or FALSE (Progress information shown)")
-  }
+  # Validate ALL.SIM.DATA
   if(!is.logical(all.sim.data)){
     stop("ALL.SIM.DATA must be either TRUE (Output all simulated data sets) or FALSE (No simulated data output")
   }
   
   # Create indicators for treatment group & cluster
+  trt = list()
+  for (arm in 1:length(nsubjects)){
+    trt[[arm]] = list()
+    for (cluster in 1:length(nsubjects[[arm]])){
+      trt[[arm]][[cluster]] = rep(arm, nsubjects[[arm]][[cluster]])
+    }
+  }
+  clust = list()
+  for (arm in 1:length(nsubjects)){
+  clust[[arm]] = list()
+    for (cluster in 1:length(nsubjects[[arm]])){
+    clust[[arm]][[cluster]] = rep(cluster, nsubjects[[arm]][[cluster]])
+    }
+  }
   
-  # Loop over nsubjects to create y arm cluster dataframe
-  
-  trt = c(rep(0, length.out = sum(nsubjects[1:nclusters[1]])), 
-          rep(1, length.out = sum(nsubjects[(nclusters[1] + 1):(nclusters[1] + nclusters[2])])))
-  clust = unlist(lapply(1:sum(nclusters), function(x) rep(x, length.out = nsubjects[x])))
+  # Create a container for the simulated.dataset output
+  sim.dat = data.frame(y = NA, trt = unlist(trt), clust = unlist(clust))
   
   # Create simulation loop
   for(i in 1:nsim){
     # Generate between-cluster effects for non-treatment and treatment
-    randint = mapply(function(nc, s, mu) stats::rnorm(nc, mean = mu, sd = sqrt(s), 
+    randint = mapply(function(nc, s, mu) stats::rnorm(nc, mean = mu, sd = sqrt(s)), 
                                                       nc = nclusters, s = sigma_b, 
                                                       mu = means)
-    
     # Create y-value
     y.bclust = vector(mode = "list", length = narms)
     y.wclust = vector(mode = "list", length = narms)
+    y = vector(mode = "list", length = narms)
     for (j in 1:narms){
-      y.bclust[j] = sapply(1:nclusters[j], function(x) rep(randint[[j]][x], length.out = nsubjects[[j]][x]))
-      y.wclust[j] = lapply(nsubjects[1:nclusters[j]], function(x) stats::rnorm(x, mean = randint[j], sd = sqrt(sigma[j])))
+      y.bclust[[j]] = sapply(1:nclusters[j], function(x) rep(randint[[j]][x], length.out = nsubjects[[j]][x]))
+      y.wclust[[j]] = lapply(nsubjects[1:nclusters[j]], function(x) stats:: rnorm(x, mean = randint[j], sd = sqrt(sigma[j])))
+      y[[j]] = y.bclust[[j]] + y.wclust[[j]]
     }
-    y.0 = y0.bclust + y0.wclust
-    
-    # Create single response vector
-    y = c(y.0, y.1)
-    
     # Create data frame for simulated dataset
-    sim.dat = data.frame(y = y, trt = trt, clust = clust)
-    if(all.sim.data == TRUE){
-      simulated.datasets = append(simulated.datasets, list(sim.dat))
-    }
+    sim.dat[["y"]] = unlist(y)
     
     # Fit GLMM (lmer)
-    if(method == 'glmm'){
-      my.mod = lme4::lmer(y ~ trt + (1|clust), data = sim.dat)
-      glmm.values = summary(my.mod)$coefficient
-      p.val = 2 * stats::pt(-abs(glmm.values['trt', 't value']), df = sum(nclusters) - 2)
-      est.vector = append(est.vector, glmm.values['trt', 'Estimate'])
-      se.vector = append(se.vector, glmm.values['trt', 'Std. Error'])
-      stat.vector = append(stat.vector, glmm.values['trt', 't value'])
-      pval.vector = append(pval.vector, p.val)
-    }
-    
-    # Fit GEE (geeglm)
-    if(method == 'gee'){
-      sim.dat = dplyr::arrange(sim.dat, clust)
-      my.mod = geepack::geeglm(y ~ trt, data = sim.dat,
-                               id = clust, corstr = "exchangeable")
-      gee.values = summary(my.mod)$coefficients
-      est.vector = append(est.vector, gee.values['trt', 'Estimate'])
-      se.vector = append(se.vector, gee.values['trt', 'Std.err'])
-      stat.vector = append(stat.vector, gee.values['trt', 'Wald'])
-      pval.vector = append(pval.vector, gee.values['trt', 'Pr(>|W|)'])
-    }
-    
-    # Update simulation progress information
-    if(quiet == FALSE){
-      if(i == 1){
-        avg.iter.time = as.numeric(difftime(Sys.time(), start.time, units = 'secs'))
-        time.est = avg.iter.time * (nsim - 1) / 60
-        hr.est = time.est %/% 60
-        min.est = round(time.est %% 60, 0)
-        message(paste0('Begin simulations :: Start Time: ', Sys.time(), 
-                       ' :: Estimated completion time: ', hr.est, 'Hr:', min.est, 'Min'))
-      }
-      
-      # Iterate progress bar
-      prog.bar$update(i / nsim)
-      Sys.sleep(1/100)
-      
-      if(i == nsim){
-        total.est = as.numeric(difftime(Sys.time(), start.time, units = 'secs'))
-        hr.est = total.est %/% 3600
-        min.est = total.est %/% 60
-        sec.est = round(total.est %% 60, 0)
-        message(paste0("Simulations Complete! Time Completed: ", Sys.time(), 
-                       "\nTotal Runtime: ", hr.est, 'Hr:', min.est, 'Min:', sec.est, 'Sec'))
-      }
-    }
+    my.mod = lme4::lmer(y ~ trt + (1|clust), data = sim.dat)
+    glmm.values = summary(my.mod)$coefficient
+    p.val = 2 * stats::pt(-abs(glmm.values['trt', 't value']), df = sum(nclusters) - 2)
+    est.vector[i] = glmm.values['trt', 'Estimate']
+    se.vector[i] = glmm.values['trt', 'Std. Error']
+    stat.vector[i] = glmm.values['trt', 't value']
+    pval.vector[i] = p.val
+    simulated.datasets[[i]] = sim.dat
   }
   
+    
   ## Output objects
-  # Create object containing summary statement
-  summary.message = paste0("Monte Carlo Power Estimation based on ", nsim, " Simulations: Simple Design, Continuous Outcome")
-  
-  # Create method object
-  long.method = switch(method, glmm = 'Generalized Linear Mixed Model', 
-                       gee = 'Generalized Estimating Equation')
-  
+    
   # Store simulation output in data frame
   cps.model.est = data.frame(Estimate = as.vector(unlist(est.vector)),
                              Std.err = as.vector(unlist(se.vector)),
@@ -268,21 +181,10 @@ cps.ma.normal = function(nsim = NULL, nsubjects = NULL, nclusters = NULL, differ
                            Lower.95.CI = round(pval.power - abs(stats::qnorm(alpha / 2)) * sqrt((pval.power * (1 - pval.power)) / nsim), 3),
                            Upper.95.CI = round(pval.power + abs(stats::qnorm(alpha / 2)) * sqrt((pval.power * (1 - pval.power)) / nsim), 3))
   
-  # Create object containing group-specific cluster sizes
-  cluster.sizes = list('Non.Treatment' = nsubjects[1:nclusters[1]], 
-                       'Treatment' = nsubjects[(nclusters[1] + 1):(nclusters[1] + nclusters[2])])
-  
-  # Create object containing number of clusters
-  n.clusters = t(data.frame("Non.Treatment" = c("n.clust" = nclusters[1]), "Treatment" = c("n.clust" = nclusters[2])))
-  
-  # Create object containing group-specific variance parameters
-  var.parms = t(data.frame('Non.Treatment' = c('ICC' = ICC[1], 'sigma' = sigma[1], 'sigma_b' = sigma_b[1]), 
-                           'Treatment' = c('ICC' = ICC[2], 'sigma' = sigma[2], 'sigma_b' = sigma_b[2])))
-  
   # Create list containing all output (class 'crtpwr') and return
-  complete.output = structure(list("overview" = summary.message, "nsim" = nsim, "power" = power.parms, "method" = long.method, "alpha" = alpha,
-                                   "cluster.sizes" = cluster.sizes, "n.clusters" = n.clusters, "variance.parms" = var.parms, 
-                                   "inputs" = difference, "model.estimates" = cps.model.est, "sim.data" = simulated.datasets), 
-                              class = 'crtpwr')
+  complete.output = list("power" = power.parms,
+                         "pval.power" = pval.power,
+                         "model.estimates" = cps.model.est, 
+                         "sim.data" = simulated.datasets)
   return(complete.output)
-  }
+}
