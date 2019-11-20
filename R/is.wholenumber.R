253772fecf22a78c427cd98abb886b8e8a019dd4
Alexandria Brown <acbro0@umass.edu>
2019-11-20 11:03:09 -0500

    is.wholenumber restored

---
+++ R/is.wholenumber.R
@@ -0,0 +1,12 @@
+#' Power simulations for cluster-randomized trials: check if whole number.
+#' 
+#' 
+#' @author Alexandria C. Sakrejda (\email{acbro0@@umass.edu} and Ken Kleinman (\email{ken.kleinman@@gmail.com})
+#' 
+#' @param x The numeric value to be tested.
+#' 
+#' @return Logical.
+#' @export
+
+is.wholenumber <- function(x, tol = .Machine$double.eps^0.5)  
+  abs(x - round(x)) < tol
