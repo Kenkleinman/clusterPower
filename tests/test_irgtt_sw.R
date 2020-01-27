9e8754892edbc5f887bc5ace02bf7f925df55b9b
Alexandria Brown <acbro0@umass.edu>
2020-01-27 16:51:38 -0500

    normal irgtt testing

--- tests/test_irgtt_sw.R
+++ tests/test_irgtt_sw.R
@@ -20,22 +20,23 @@ test_check("clusterPower")
 context("IRGTT outcome accuracy")
 
 # compare analytic to simulation methods
-test_that("analytic normal irgtt case matches simulated method irgtt case", {
-  expect_equal(as.numeric(cps.irgtt.normal(nsim = 100, 
-                                           nsubjects = c(100, 10),
-                                           nclusters = 15, 
-                                           difference = .5,
-                                           sigma_sq = 1, 
-                                           sigma_sq2 = 0.9, 
-                                           sigma_b_sq2 = 0.1, 
+test_that("analytic normal irgtt case matches simulated method normal irgtt case", {
+  expect_equal(signif(as.numeric(cps.irgtt.normal(nsim = 100, 
+                                           nsubjects = c(400, 10),
+                                           nclusters = 30, 
+                                           difference = 0.5,
+                                           sigma_sq = 4, 
+                                           sigma_sq2 = 1, 
+                                           sigma_b_sq2 = 0.5, 
                                            alpha = 0.05,
                                            quiet = FALSE, 
-                                           all.sim.data = FALSE)), 
-               as.numeric(cpa.irgtt.normal(nclusters = 100, 
-                                           nsubjects = c(100, 10), 
+                                           all.sim.data = FALSE,
+                                           seed = 123)$power$Power), 1), 
+               as.numeric(signif(cpa.irgtt.normal(nclusters = 30, 
+                                           nsubjects = 10, 
                                            d = 0.5, 
-                                           varu = 0.1, 
-                                           vare = 0.9, 
-                                           varr = 1, 
-                                           ncontrols = 77.81, 
-                                           power = NA)))})
+                                           varu = 0.5, 
+                                           vare = 1, 
+                                           varr = 4, 
+                                           ncontrols = 400, 
+                                           power = NA), 1)))})
