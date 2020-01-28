49be6425905c4a58d1db0f7cf475722872055fcd
Alexandria Brown <acbro0@umass.edu>
2020-01-28 16:41:47 -0500

    irgtt testing continued

--- tests/test_irgtt_sw.R
+++ tests/test_irgtt_sw.R
@@ -19,7 +19,7 @@ test_check("clusterPower")
 
 context("IRGTT outcome accuracy")
 
-# compare analytic to simulation methods
+# compare analytic to simulation methods (normal)
 test_that("analytic normal irgtt case matches simulated method normal irgtt case", {
   expect_equal(signif(as.numeric(cps.irgtt.normal(nsim = 100, 
                                            nsubjects = c(400, 10),
@@ -40,3 +40,76 @@ test_that("analytic normal irgtt case matches simulated method normal irgtt case
                                            varr = 4, 
                                            ncontrols = 400, 
                                            power = NA), 1)))})
+
+
+# compare simulation method (normal) to a constant
+test_that("simulation normal irgtt case matches a constant", {
+  expect_equal(signif(as.numeric(cps.irgtt.normal(nsim = 100, 
+                                                  nsubjects = c(550, 12),
+                                                  nclusters = 125, 
+                                                  difference = 0.25,
+                                                  sigma_sq = 3, 
+                                                  sigma_sq2 = 2.1, 
+                                                  sigma_b_sq2 = 0.25, 
+                                                  alpha = 0.05,
+                                                  quiet = FALSE, 
+                                                  all.sim.data = FALSE,
+                                                  seed = 123)$power$Power), 2),
+               0.78)})
+
+# compare analytic to a constant (normal)
+test_that("analytic normal irgtt case matches a constant", {
+  expect_equal(as.numeric(signif(cpa.irgtt.normal(nclusters = 125, 
+                                                  nsubjects = 12, 
+                                                  d = 0.25, 
+                                                  varu = 0.25, 
+                                                  vare = 2.1, 
+                                                  varr = 3, 
+                                                  ncontrols = 550, 
+                                                  power = NA), 2)),
+               0.76)})
+
+
+# compare analytic to simulation methods (binary)
+test_that("analytic binary irgtt case matches simulated method binary irgtt case", {
+  expect_equal(signif(as.numeric(cps.irgtt.binary(nsim = 100, 
+                                                  nsubjects = 30, 
+                                                  nclusters = 10, 
+                                                  p1 = 0.2,
+                                                  p2 = 0.5, 
+                                                  sigma_b_sq2 = 1, 
+                                                  alpha = 0.05, 
+                                                  all.sim.data = FALSE, 
+                                                  seed = 123)$power$Power), 1), 
+               as.numeric(signif(cpa.irgtt.binary(nclusters = 10, 
+                                                  nsubjects = 30,
+                                                  ncontrols = 30, 
+                                                  icc = 0.25, 
+                                                  p.e = 0.5, 
+                                                  p.c = 0.2, 
+                                                  power = NA), 1)))})
+
+# compare simulation methods (binary) to a constant
+test_that("simulated method binary irgtt case matches a constant", {
+  expect_equal(signif(as.numeric(cps.irgtt.binary(nsim = 100, 
+                                                  nsubjects = c(300, 12), 
+                                                  nclusters = 120, 
+                                                  p1 = 0.3,
+                                                  p2 = 0.2, 
+                                                  sigma_b_sq2 = 1.75, 
+                                                  alpha = 0.05, 
+                                                  all.sim.data = FALSE, 
+                                                  seed = 123)$power$Power), 2), 
+               0.81)})
+
+
+# compare analytic methods (binary) to a constant
+test_that("analytic binary irgtt case matches a constant", {
+  expect_equal(as.numeric(signif(cpa.irgtt.binary(nclusters = 120, 
+                                                  nsubjects = 12,
+                                                  ncontrols = 300, 
+                                                  icc = 0.4, 
+                                                  p.e = 0.2, 
+                                                  p.c = 0.3, 
+                                                  power = NA), 2)),
+               0.79)})
