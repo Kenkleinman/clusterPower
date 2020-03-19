3ddd274da1273b8f0395dccd9e5763ae562479f5
Alexandria Brown <acbro0@umass.edu>
2020-03-19 16:23:22 -0400

    method update

--- R/summary.method.crtpwr.R
+++ R/summary.method.crtpwr.R
@@ -10,21 +10,10 @@ setMethod("summary",
       cat(paste("\nMethod:", object[['method']], "\n"))
       cat("\nVariance Parameters:\n")
       print(object[['variance.parms']])
-      cat("\nCluster Sizes:\n")
-  #    if(length(object[['cluster.sizes']][[1]]) < 10 & length(object[['cluster.sizes']][[2]]) < 10){
-  #      cat(paste0("  Non-Treatment (n = ", object[['n.clusters']][1], "): ", format(object[['cluster.sizes']][[1]]), "\n"))
-  #      cat(paste0("  Treatment (n = ", object[['n.clusters']][2], "): ", format(object[['cluster.sizes']][[2]]), "\n"))
-  #    }else{
-  #      cat("   Non-Treatment (n =", object[['n.clusters']][1], "):", format(object[['cluster.sizes']][[1]][1:10]), "...\n", sep = " ") 
-  #      cat("   Treatment (n =", object[['n.clusters']][2], "):", format(object[['cluster.sizes']][[2]][1:10]), "...\n", sep = " ") 
-  #    }
-      cat("\nArms:")
-      print(table(object[['narms']]))
-      cat("\nClusters:")
-      print(table(object[['n.clusters']]))
-      print(table(object[['cluster.sizes']]))
-      cat("\nArms:")
-      print(table(object[['narms']]))
+      cat("\nClusters:\n")
+      print(object[['n.clusters']])
+      cat("\nObservations:\n")
+      print(object[['cluster.sizes']])
       cat("\nConvergence:")
       print(table(object[['convergence']]))
     }
