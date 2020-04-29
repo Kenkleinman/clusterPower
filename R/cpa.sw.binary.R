cd02328ad027d5b4794f6f6bd14ac3da7c44dafe
Alexandria Brown <acbro0@umass.edu>
2020-04-29 16:26:15 -0400

    Ready to merge

--- R/cpa.sw.binary.R
+++ R/cpa.sw.binary.R
@@ -1,58 +1,59 @@
 #' Power simulations for cluster-randomized trials: Stepped Wedge Design, Binary Outcome
 #'
-#' This function uses a modified Cox method to determine power for stepped 
-#' wedge cluster-randomized controlled trials. Users can modify a variety of 
+#' This function uses a modified Cox method to determine power for stepped
+#' wedge cluster-randomized controlled trials. Users can modify a variety of
 #' parameters to suit their desired experimental situation.
-#' 
+#'
 #' The stepped wedge trial design is a type of cross-over
-#' design in which clusters change treatments in waves. Initially all the 
+#' design in which clusters change treatments in waves. Initially all the
 #' clusters recieve the same standard treatment, and at the end of the trial all
-#' of the clusters will be recieving the treatment of interest. More than one 
-#' cluster can change treatments in a wave, but the order in which clusters 
-#' change treatments is randomly determined. The outcome of interest is assessed 
+#' of the clusters will be recieving the treatment of interest. More than one
+#' cluster can change treatments in a wave, but the order in which clusters
+#' change treatments is randomly determined. The outcome of interest is assessed
 #' in each cluster during each wave.
-#' 
-#' Users must specify the number of subjects per cluster, number of clusters, 
-#' the number of time steps, the baseline effect, the expected treatment effect, 
-#' expected absolute difference between treatment 
+#'
+#' Users must specify the number of subjects per cluster, number of clusters,
+#' the number of time steps, the baseline effect, the expected treatment effect,
+#' expected absolute difference between treatment
 #' arms, ICC, and time effect.
-#' 
-#' Fortran for this package was kindly provided by Dr. Zhou and others. 
-#' 
-#' @param nsubjects Number of subjects per cluster; accepts a scalar. Equal cluster sizes 
+#'
+#' Fortran for this package was kindly provided by Dr. Zhou and others.
+#'
+#' @param nsubjects Number of subjects per cluster; accepts a scalar. Equal cluster sizes
 #' are assumed (required).
 #' @param nclusters Number of clusters; accepts non-negative integer scalar (required).
-#' @param steps Number of crossover steps; Accepts positive scalar indicating the total 
+#' @param steps Number of crossover steps; Accepts positive scalar indicating the total
 #' number of steps (required).
-#' @param d Total change over the study period (assume that time effects are linear 
+#' @param d Total change over the study period (assume that time effects are linear
 #' across time steps); accepts numeric (required).
 #' @param ICC ICC for participants at first time step; accepts numeric (required).
 #' @param beta Estimated treatment effect; accepts numeric (required).
 #' @param mu Estimated baseline effect; accepts numeric (required).
-#' @param tol Machine tolerance. Accepts numeric. Default is 1e-5. 
-#' @param GQ Number of quadriture points used in Gaussian Legendre integration; accepts 
+#' @param tol Machine tolerance. Accepts numeric. Default is 1e-5.
+#' @param GQ Number of quadriture points used in Gaussian Legendre integration; accepts
 #' a scalar. Default is 100.
-#' 
+#' @param quiet Suppresses the progress bar; logical. Default is FALSE.
+#'
 #' @return The estimated power.
-#' 
-#' @examples 
+#'
+#' @examples
 #' \dontrun{
-#' holder <- cpa.sw.binary(nclusters = 16, 
-#'   steps = 3, 
-#'   nsubjects = 90, 
-#'   d = -0.01, 
-#'   ICC = 0.01, 
-#'   beta = -0.05, 
-#'   mu = 0.18, 
-#'   tol = 1e-5, 
+#' holder <- cpa.sw.binary(nclusters = 16,
+#'   steps = 3,
+#'   nsubjects = 90,
+#'   d = -0.01,
+#'   ICC = 0.01,
+#'   beta = -0.05,
+#'   mu = 0.18,
+#'   tol = 1e-5,
 #'   GQ = 100)
 #' }
 #'
 #' @author Alexandria C. Sakrejda (\email{acbro0@@umass.edu})
 #' @author Ken Kleinman (\email{ken.kleinman@@gmail.com})
 #'
-#' @references Zhou X, Liao X, Kunz LM, Normand ST, Wang M, Spiegelman D. A maximum 
-#' likelihood approach to power calculations for stepped wedge designs of binary 
+#' @references Zhou X, Liao X, Kunz LM, Normand ST, Wang M, Spiegelman D. A maximum
+#' likelihood approach to power calculations for stepped wedge designs of binary
 #' outcomes. Biostatistics. 2020 Jan 1;21(1):102-121. doi: 10.1093/biostatistics/kxy031
 #' @export
 cpa.sw.binary <- function(nclusters = NA,
@@ -65,6 +66,7 @@ cpa.sw.binary <- function(nclusters = NA,
                           tol = 1e-5,
                           GQ = 100,
                           quiet = FALSE) {
+  
   ###### Define some FORTRAN-calling functions  ########
   
   syminverse <- function(invVar = invVar,
@@ -212,7 +214,49 @@ cpa.sw.binary <- function(nclusters = NA,
       return(o)
     }
   
+  ###############################################
   ######## main function code ###################
+  ###############################################
+  
+  ## Validate user entries
+  if (!is.integer(nclusters) ||
+      nclusters < 1 ||
+      length(nclusters) > 1 ||
+      is.na(nclusters)) {
+    errorCondition(message = "nclusters must be a positive scalar.")
+  }
+  if (!is.integer(steps) ||
+      steps < 1 ||
+      length(steps) > 1 ||
+      is.na(steps)) {
+    errorCondition(message = "steps must be a positive scalar.")
+  }
+  if (!is.integer(nsubjects) ||
+      nsubjects < 1 ||
+      length(nsubjects) > 1 ||
+      is.na(nsubjects)) {
+    errorCondition(message = "nsubjects must be a positive scalar.")
+  }
+  if (!is.integer(GQ) ||
+      GQ < 1 ||
+      length(GQ) > 1 ||
+      is.na(GQ)) {
+    errorCondition(message = "GQ must be a positive scalar.")
+  }
+  if (is.na(d) ||
+      is.na(ICC) || is.na(beta) || is.na(mu) || is.na(tol)) {
+    errorCondition("User must provide a value for d, ICC, beta, mu, and tol. See documentation for details.")
+  }
+  if (!is.logical(quiet)) {
+    errorCondition("Provide a logical for quiet.")
+  }
+  
+#### cpa.sw.binary R code ##################
+  
+  #  Update progress information
+  if (quiet == FALSE) {
+    message(paste0('Begin calculations :: Start Time: ', Sys.time()))
+  }
   
   p0 <- rep(mu, times = steps)
   p11 <-  p0[1] + beta
@@ -292,20 +336,20 @@ cpa.sw.binary <- function(nclusters = NA,
         interventionX[i, j] <- 1
       }
     }
-    
     invVar <- matrix(0, nrow = (steps + 2), ncol = (steps + 2))
     
     h <- 0
-    
     ## Set start.time for progress iterator & initialize progress bar
-    start.time = Sys.time()
-    prog.bar =  progress::progress_bar$new(
-      format = "(:spin) [:bar] :percent eta :eta",
-      total = (nsubjects + 1),
-      clear = FALSE,
-      width = 100
-    )
-    prog.bar$tick(0)
+    if (quiet == FALSE) {
+      start.time = Sys.time()
+      prog.bar =  progress::progress_bar$new(
+        format = "(:spin) [:bar] :percent eta :eta",
+        total = (nsubjects + 1),
+        clear = FALSE,
+        width = 100
+      )
+      prog.bar$tick(0)
+    }
     
     for (i in 1:(steps - 1)) {
       z0 <- rep(0, times = steps)
@@ -313,7 +357,7 @@ cpa.sw.binary <- function(nclusters = NA,
       h <- h
       while (finish < 1) {
         h <- h + 1
-        XX <- interventionX[i,]
+        XX <- interventionX[i, ]
         z1 <- nsubjects - z0
         
         Dholder <- der_likelihood_time(
@@ -334,69 +378,42 @@ cpa.sw.binary <- function(nclusters = NA,
           t = t,
           wts = wts
         )
-        
-        
         prob <- Dholder$prob
         derlikelihood <- Dholder$derlikelihood
-        
         VecHolder <-
           vectorsquare(derlikelihood = derlikelihood, steps = steps)
         derlikelihood2 <- VecHolder$c
-        
         invVar <- invVar + derlikelihood2 * prob
         
-        #  Update simulation progress information
+        # Iterate progress bar
+        totaliter <- ((nsubjects + 1) ^ steps * (steps - 1))
         if (quiet == FALSE) {
-          if (h == 9) {
-            avg.iter.time = as.numeric(difftime(Sys.time(), start.time, units = 'secs'))
-            time.est = avg.iter.time * ((nsubjects + 1) ^ steps * (steps - 1)) - 10
-            hr.est = time.est %/% 60
-            min.est = round(time.est %% 60, 0)
-            message(paste0('Begin calculations :: Start Time: ', Sys.time()))
-          }
-          # Iterate progress bar
-          hspecial <- (h / 100)
-          if (hspecial == as.integer(hspecial)) {
-            prog.bar$update(h / ((nsubjects + 1) ^ steps * (steps - 1)))
+          hspecial <- (h / 100000)
+          if (hspecial == as.integer(hspecial) ||
+              hspecial == totaliter ||
+              h == 10) {
+            prog.bar$update(h / totaliter)
             Sys.sleep(1 / 100)
           }
-          
-          #update the counter
-          finish <- 0
-          z0[1] <- z0[1] + 1
-          for (p in 1:(steps - 1)) {
-            if (z0[p] > nsubjects) {
-              z0[p] <- 0
-              z0[p + 1] <- z0[p + 1] + 1
-            } else {
-              break
-            }
+        }
+        
+        #update the counter
+        finish <- 0
+        z0[1] <- z0[1] + 1
+        for (p in 1:(steps - 1)) {
+          if (z0[p] > nsubjects) {
+            z0[p] <- 0
+            z0[p + 1] <- z0[p + 1] + 1
+          } else {
+            break
           }
-          
-          if (z0[steps] > nsubjects) {
-            finish = 1
-          }
+        }
+        
+        if (z0[steps] > nsubjects) {
+          finish = 1
         }
       }
     }
-    ## wrap up progress bar
-    total.est = as.numeric(difftime(Sys.time(), start.time, units = 'secs'))
-    hr.est = total.est %/% 3600
-    min.est = total.est %/% 60
-    sec.est = round(total.est %% 60, 0)
-    message(
-      paste0(
-        "Calculations Complete! Time Completed: ",
-        Sys.time(),
-        "\nTotal Runtime: ",
-        hr.est,
-        'Hr:',
-        min.est,
-        'Min:',
-        sec.est,
-        'Sec'
-      )
-    )
     
     mat <- matrix(0, nrow = (steps + 2), ncol = (steps + 2))
     mat <- syminverse(invVar = invVar,
@@ -408,10 +425,10 @@ cpa.sw.binary <- function(nclusters = NA,
       ncol = (steps + 2),
       byrow = FALSE
     )
-    
     sebeta <- sqrt(Var[2, 2] / DD)
     power <- pnorm(beta / sebeta - 1.959964, lower.tail = TRUE) +
       pnorm(-beta / sebeta - 1.959964, lower.tail = TRUE)
+    
   } else {
     if (beta > 0) {
       a = -mu
@@ -420,7 +437,9 @@ cpa.sw.binary <- function(nclusters = NA,
       a = -mu - beta
       b = 1 - mu
     }
-    quadholder <- legendre_handle(order = GQ, a = a, b = b)
+    quadholder <- legendre_handle(order = GQ,
+                                  a = a,
+                                  b = b)
     t <- quadholder$x
     wts <- quadholder$w
     
@@ -439,6 +458,26 @@ cpa.sw.binary <- function(nclusters = NA,
         GQW = wts
       )
     power <- Linpower$power
+  }
+  if (quiet == FALSE) {
+    ## show elapsed time
+    total.est = as.numeric(difftime(Sys.time(), start.time, units = 'secs'))
+    hr.est = total.est %/% 3600
+    min.est = total.est %/% 60
+    sec.est = round(total.est %% 60, 0)
+    message(
+      paste0(
+        "Time Completed: ",
+        Sys.time(),
+        "\nTotal Runtime: ",
+        hr.est,
+        'Hr:',
+        min.est,
+        'Min:',
+        sec.est,
+        'Sec'
+      )
+    )
   }
   return(power)
 }
