5492d9c79a619692a03b7abf72c8a036021c5ff1
Alexandria Brown <acbro0@umass.edu>
2020-05-21 13:52:29 -0400

    stub help pages added

---
+++ R/cpa.irgtt.count.R
@@ -0,0 +1,10 @@
+#' This help page is a stub. Equations do not yet exist for this type of analysis and outcome.
+#' Try \code{cps.did.count()} instead.
+#'  
+#' @author Alexandria C. Sakrejda (\email{acbro0@@umass.edu}
+#' @author Ken Kleinman (\email{ken.kleinman@@gmail.com})
+#' 
+#' @export
+ cpa.irgtt.count <- function(x) {
+warning("No method exists for cpa.irgtt.count. See ?cpa.irgtt.count for details.")
+}
---
+++ R/cpa.ma.count.R
@@ -0,0 +1,10 @@
+#' This help page is a stub. Equations do not yet exist for this type of analysis and outcome.
+#' Try \code{cps.did.count()} instead.
+#' 
+#' @author Alexandria C. Sakrejda (\email{acbro0@@umass.edu}
+#' @author Ken Kleinman (\email{ken.kleinman@@gmail.com})
+#' 
+#' @export
+cpa.ma.count <- function(x) {
+  warning("No method exists for cpa.ma.count. See ?cpa.ma.count for details.")
+}
