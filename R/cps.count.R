8530603b5968b7ad69c620f68cad35e0dd639460
Alexandria Brown <acbro0@umass.edu>
2020-06-24 15:43:31 -0400

    More updates and formatting fixes

--- R/cps.count.R
+++ R/cps.count.R
@@ -6,63 +6,63 @@
 #' desired experimental situation.
 #' 
 #' Runs the power simulation for count outcomes.
-#' 
-#' Users must specify the desired number of simulations, number of subjects per 
-#' cluster, number of clusters per treatment arm, between-cluster variance, 
-#' two of the following: expected count in non-treatment group, expected count 
-#' in treatment group, difference in counts between groups; significance level, 
-#' analytic method, and whether or not progress updates should be displayed 
+#'
+#' Users must specify the desired number of simulations, number of subjects per
+#' cluster, number of clusters per treatment arm, between-cluster variance,
+#' two of the following: expected count in non-treatment group, expected count
+#' in treatment group, difference in counts between groups; significance level,
+#' analytic method, and whether or not progress updates should be displayed
 #' while the function is running.
-#' 
-#' Non-convergent models are not included in the calculation of exact confidence 
+#'
+#' Non-convergent models are not included in the calculation of exact confidence
 #' intervals.
-#' 
-#' @section Testing details:   
-#' This function has been verified, where possible, against reference values from PASS11, 
-#' \code{CRTsize::n4incidence}, \code{clusterPower::cps.count}, and 
+#'
+#' @section Testing details:
+#' This function has been verified, where possible, against reference values from PASS11,
+#' \code{CRTsize::n4incidence}, \code{clusterPower::cps.count}, and
 #' \code{clusterPower::cpa.count}.
-#' 
+#'
 #' @param nsim Number of datasets to simulate; accepts integer (required).
-#' @param nsubjects Number of subjects per cluster; accepts a single integer or a vector 
-#' of 2 integers (if nsubjects differs between arms) (required). 
-#' @param nclusters Number of clusters per treatment group; accepts a single integer 
+#' @param nsubjects Number of subjects per cluster; accepts a single integer or a vector
+#' of 2 integers (if nsubjects differs between arms) (required).
+#' @param nclusters Number of clusters per treatment group; accepts a single integer
 #' or a vector of 2 integers (if nsubjects differs between arms) (required).
 #' At least 2 of the following 3 arguments must be specified:
 #' @param c1 Expected outcome count in non-treatment group
 #' @param c2 Expected outcome count in treatment group
-#' @param c.diff Expected difference in outcome count between groups, defined as 
+#' @param c.diff Expected difference in outcome count between groups, defined as
 #' c.diff = c1 - c2
-#' @param sigma_b_sq Between-cluster variance; if sigma_b_sq2 is not specified, 
+#' @param sigma_b_sq Between-cluster variance; if sigma_b_sq2 is not specified,
 #' between cluster variances are assumed to be equal between groups. Accepts numeric
-#' If between cluster variances differ between treatment groups, the following must 
+#' If between cluster variances differ between treatment groups, the following must
 #' also be specified:
 #' @param sigma_b_sq2 Between-cluster variance for clusters in TREATMENT group
-#' @param family Distribution from which responses are simulated. Accepts Poisson 
+#' @param family Distribution from which responses are simulated. Accepts Poisson
 #' ('poisson') or negative binomial ('neg.binom') (required); default = 'poisson'
-#' @param analysis Family used for regression; currently only applicable for GLMM. 
+#' @param analysis Family used for regression; currently only applicable for GLMM.
 #' Accepts c('poisson', 'neg.binom') (required); default = 'poisson'
-#' @param method Analytical method, either Generalized Linear Mixed Effects Model 
-#' (GLMM) or Generalized Estimating Equation (GEE). Accepts c('glmm', 'gee') 
+#' @param method Analytical method, either Generalized Linear Mixed Effects Model
+#' (GLMM) or Generalized Estimating Equation (GEE). Accepts c('glmm', 'gee')
 #' (required); default = 'glmm'
 #' @param alpha Significance level. Default = 0.05.
-#' @param quiet When set to FALSE, displays simulation progress and estimated 
+#' @param quiet When set to FALSE, displays simulation progress and estimated
 #' completion time. Default = FALSE.
 #' @param all.sim.data Option to output list of all simulated datasets. Default = FALSE
 #' @param seed Option to set the seed. Default is NA.
-#' @param nofit Option to skip model fitting and analysis and return the simulated data. 
-#' Defaults to \code{FALSE}. 
-#' @param optimizer Option to fit with a different optimizer (using the package 
+#' @param nofit Option to skip model fitting and analysis and return the simulated data.
+#' Defaults to \code{FALSE}.
+#' @param optimizer Option to fit with a different optimizer (using the package
 #' \code{optimx}). Defaults to L-BFGS-B.
-#' 
+#'
 #' @return A list with the following components
 #' \itemize{
-#'   \item Character string indicating total number of simulations, distribution of 
+#'   \item Character string indicating total number of simulations, distribution of
 #'   simulated data, and regression family
 #'   \item Number of simulations
-#'   \item Data frame with columns "Power" (Estimated statistical power), 
-#'                "lower.95.ci" (Lower 95% confidence interval bound), 
+#'   \item Data frame with columns "Power" (Estimated statistical power),
+#'                "lower.95.ci" (Lower 95% confidence interval bound),
 #'                "upper.95.ci" (Upper 95% confidence interval bound).
-#'                Note that non-convergent models are returned for review, 
+#'                Note that non-convergent models are returned for review,
 #'                but not included in this calculation.
 #'   \item Analytic method used for power estimation
 #'   \item Data frame containing families for distribution and analysis of simulated data
@@ -71,28 +71,28 @@
 #'   \item Vector containing user-defined number of clusters
 #'   \item Data frame reporting between-cluster variances for Treatment/Non-Treatment groups
 #'   \item Vector containing expected counts and risk ratios based on user inputs
-#'   \item Data frame with columns: 
-#'                   "Estimate" (Estimate of treatment effect for a given simulation), 
-#'                   "Std.err" (Standard error for treatment effect estimate), 
-#'                   "Test.statistic" (z-value (for GLMM) or Wald statistic (for GEE)), 
+#'   \item Data frame with columns:
+#'                   "Estimate" (Estimate of treatment effect for a given simulation),
+#'                   "Std.err" (Standard error for treatment effect estimate),
+#'                   "Test.statistic" (z-value (for GLMM) or Wald statistic (for GEE)),
 #'                   "p.value",
 #'                   "sig.val" (Is p-value less than alpha?)
-#'   \item List of data frames, each containing: 
-#'                   "y" (Simulated response value), 
-#'                   "trt" (Indicator for treatment group), 
+#'   \item List of data frames, each containing:
+#'                   "y" (Simulated response value),
+#'                   "trt" (Indicator for treatment group),
 #'                   "clust" (Indicator for cluster)
 #' }
-#' @author Alexander R. Bogdan 
+#' @author Alexander R. Bogdan
 #' @author Alexandria C. Sakrejda (\email{acbro0@@umass.edu}
 #' @author Ken Kleinman (\email{ken.kleinman@@gmail.com})
 #'
-#' 
-#' @examples 
+#'
+#' @examples
 #' \dontrun{
-#' count.sim = cps.count(nsim = 100, nsubjects = 20, nclusters = 10, 
-#'                       c1 = 20, c2 = 30, sigma_b_sq = 0.1, 
+#' count.sim = cps.count(nsim = 100, nsubjects = 20, nclusters = 10,
+#'                       c1 = 20, c2 = 30, sigma_b_sq = 0.1,
 #'                       family = 'poisson', analysis = 'poisson',
-#'                       method = 'glmm', alpha = 0.05, quiet = FALSE, 
+#'                       method = 'glmm', alpha = 0.05, quiet = FALSE,
 #'                       all.sim.data = FALSE, seed = 123, optimizer = "L-BFGS-B")
 #' }
 #'
@@ -183,7 +183,8 @@ cps.count = function(nsim = NULL,
     nsubjects = rep(nsubjects, 2)
   }
   if (length(nclusters) == 2 &&
-      length(nsubjects) != 1 && length(nsubjects) != length(nclusters)) {
+      length(nsubjects) != 1 &&
+      length(nsubjects) != length(nclusters)) {
     stop(
       "A cluster size must be specified for each cluster. If all cluster sizes are equal, please provide a single value for NSUBJECTS"
     )
